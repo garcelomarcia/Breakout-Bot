1c4b83be961fb45fe94e41271a2242c5009f7701
garcelomarcia <marcelogarcia91@gmail.com>
2022-08-02 21:19:02 -0500

    added Hello World

--- app.py
+++ app.py
@@ -30,5 +30,8 @@ def webhook():
         "message": data
     }
 
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debut=True)
