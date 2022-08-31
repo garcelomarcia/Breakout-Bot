4ddbbebd89c22936119b851485cec5eaa3458c1d
garcelomarcia <marcelogarcia91@gmail.com>
2022-08-31 12:05:24 -0500

    fixed market orders

--- app.py
+++ app.py
@@ -50,13 +50,16 @@ def entry_order(side, quantity,symbol,price, opp_side, tp,sl):
                 open_order = False
     except Exception as e:
         print("an exception occured - {}".format(e))
-        print("sending order at market price")
-        order = client.futures_create_order(symbol=symbol, side=side, type='MARKET', quantity=quantity)
-        time.sleep(0.5)
-        print(f"sending order: Take Profit Order {opp_side}{quantity}{symbol} @{tp}")
-        tp_order = client.futures_create_order(symbol=symbol, side=opp_side, type='LIMIT', quantity=quantity, price=tp, reduceOnly=True, timeInForce="GTC")
-        print(f"sending order: Stop Loss {opp_side}{quantity}{symbol} @{sl}")
-        sl_order = client.futures_create_order(symbol=symbol, side=opp_side, type='STOP_MARKET', quantity=quantity, stopPrice=sl, reduceOnly=True, timeInForce="GTC")
+        if not client.futures_get_open_orders(symbol=symbol) and float(client.futures_position_information(symbol=symbol)[0]['positionAmt']) == 0.0:
+            print("sending order at market price")
+            order = client.futures_create_order(symbol=symbol, side=side, type='MARKET', quantity=quantity)
+            time.sleep(0.5)
+            print(f"sending order: Take Profit Order {opp_side}{quantity}{symbol} @{tp}")
+            tp_order = client.futures_create_order(symbol=symbol, side=opp_side, type='LIMIT', quantity=quantity, price=tp, reduceOnly=True, timeInForce="GTC")
+            print(f"sending order: Stop Loss {opp_side}{quantity}{symbol} @{sl}")
+            sl_order = client.futures_create_order(symbol=symbol, side=opp_side, type='STOP_MARKET', quantity=quantity, stopPrice=sl, reduceOnly=True, timeInForce="GTC")
+        else: 
+            return False
     
     
            
