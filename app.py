3115eb2fa3451ed3238c54f223924e34ea7de249
garcelomarcia <marcelogarcia91@gmail.com>
2022-09-11 19:24:33 -0500

    jjjjasñledkgjga

--- app.py
+++ app.py
@@ -30,14 +30,21 @@ def entry_order(side, quantity,symbol,price, opp_side, tp,sl):
     try:    
         print(f"sending order: Stop Market Order {side}{quantity}{symbol} @{price}")
         order = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET', quantity=quantity, stopPrice=price)
-        time.sleep(1)        
+        time.sleep(0.5)        
         order_executed = False
         while order_executed == False:
             if float(client.futures_position_information(symbol=symbol)[0]['positionAmt']) != 0.0:            
-                print(f"sending order: Take Profit Order {opp_side}{quantity}{symbol} @{tp}")
-                tp_order = client.futures_create_order(symbol=symbol, side=opp_side, type='LIMIT', quantity=quantity, price=tp, reduceOnly=True, timeInForce="GTC")
-                print(f"sending order: Stop Loss {opp_side}{quantity}{symbol} @{sl}")
-                sl_order = client.futures_create_order(symbol=symbol, side=opp_side, type='STOP_MARKET', quantity=quantity, stopPrice=sl, reduceOnly=True, timeInForce="GTC")
+                try:
+                    print(f"sending order: Take Profit Order {opp_side}{quantity}{symbol} @{tp}")
+                    tp_order = client.futures_create_order(symbol=symbol, side=opp_side, type='LIMIT', quantity=quantity, price=tp, reduceOnly=True, timeInForce="GTC")
+                    print(f"sending order: Stop Loss {opp_side}{quantity}{symbol} @{sl}")
+                    sl_order = client.futures_create_order(symbol=symbol, side=opp_side, type='STOP_MARKET', quantity=quantity, stopPrice=sl, reduceOnly=True, timeInForce="GTC")
+                except BinanceAPIException as err:
+                    if str(err.message) == "Order would immediately trigger.":
+                        print(f"Exiting trade at Market Price")
+                        client.futures_create_order(symbol=symbol, side=opp_side, type='MARKET', quantity=quantity, reduceOnly=True)
+                        client.futures_cancel_all_open_orders(symbol=symbol)
+                    return False
                 order_executed = True
                 break
             else:
@@ -58,6 +65,7 @@ def entry_order(side, quantity,symbol,price, opp_side, tp,sl):
                 if str(err.message) == "Order would immediately trigger.":
                     print(f"Exiting trade at Market Price")
                     client.futures_create_order(symbol=symbol, side=opp_side, type='MARKET', quantity=quantity, reduceOnly=True)
+                    client.futures_cancel_all_open_orders(symbol=symbol)
                 return False
                 
 
